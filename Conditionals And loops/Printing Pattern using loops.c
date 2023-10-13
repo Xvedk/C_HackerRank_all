a8d9de61e51c19b5aae48fc1fa53b89d757c349c
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2023-10-13 20:19:40 +0530

    Create Printing Pattern using loops.c

---
+++ Conditionals And loops/Printing Pattern using loops.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+
+{
+    int i,j,k;
+    int n,m;
+    scanf("%d",&n);
+    m=2*n+1;
+    for(i=1;i<=n;i++)
+    {
+        for(j=n;j>n-i;j--)
+        {
+             printf("%d ",j);
+        }
+        for(j=1;j<=n-i;j++)
+        {
+            printf("%d ",n-i+1);
+        }
+        for(j=n-i;j>=1;j--)
+        {
+            printf("%d ",n-i+1);
+        }
+        k=n-i+2;
+        for(j=1;j<=i-1;j++)
+        {
+            printf("%d ",k), k++;
+        }
+        printf("\n");
+    }
+    for(i=1;i<n;i++)
+    {
+        for(j=n;j>=i+1;j--)
+        {
+            printf("%d ",j);
+        }
+        for(j=1;j<=2*i-1;j++)
+        {
+            printf("%d ",i+1);
+        }
+        for(j=i+1;j<=n;j++)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
