2d961321bc3b92737c78470dca73901106a4dcba
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2023-10-13 20:17:42 +0530

    Create For Loop in C.c

---
+++ Conditionals And loops/For Loop in C.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+    int i,j,k;
+    int a,b;
+    char *ch[]={"null","one","two","three","four","five",
+    "six","seven","eight","nine"};
+    scanf("%d%d",&a,&b);
+
+    for(i=a;i<=b;i++)
+    {
+        if(i>9 && i%2)
+            printf("odd\n");
+        else if(i>9 && i%2==0)
+            printf("even\n");
+        else
+            printf("%s\n",ch[i]);
+
+    }
+    return 0;
+}
