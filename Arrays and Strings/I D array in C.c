c22e8b8ac8ba153a2161957762bdee15664fce55
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2023-10-14 10:56:56 +0530

    Update I D array in C.c

--- Arrays and Strings/I D array in C.c
+++ Arrays and Strings/I D array in C.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n); // Read the size of the array
+
+    // Dynamically allocate memory for the array
+    int *arr = (int *)malloc(n * sizeof(int));
+
+    // Check if memory allocation was successful
+    if (arr == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1; // Exit with an error code
+    }
+
+    // Read integers into the array
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Calculate the sum of the elements
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    // Print the sum
+    printf("%d\n", sum);
+
+    // Free the dynamically allocated memory
+    free(arr);
+
+    return 0; // Exit with success
+}
 
