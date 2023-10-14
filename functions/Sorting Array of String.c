e4185de5e7d86a3f79807eed0e46c7b3115d9968
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2023-10-15 01:24:44 +0530

    Sorting Array of String.c

---
+++ functions/Sorting Array of String.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int lexicographic_sort(const char* a, const char* b) {
+    return strcmp(a, b);
+}
+
+int lexicographic_sort_reverse(const char* a, const char* b) {
+    return strcmp(b, a);
+}
+
+int sort_by_number_of_distinct_characters(const char* a, const char* b) {
+    int char_count_a[26] = {0};
+    int char_count_b[26] = {0};
+
+    for (int i = 0; a[i] != '\0'; i++) {
+        char_count_a[a[i] - 'a'] = 1;
+    }
+
+    for (int i = 0; b[i] != '\0'; i++) {
+        char_count_b[b[i] - 'a'] = 1;
+    }
+
+    int count_a = 0, count_b = 0;
+
+    for (int i = 0; i < 26; i++) {
+        count_a += char_count_a[i];
+        count_b += char_count_b[i];
+    }
+
+    if (count_a != count_b) {
+        return count_a - count_b;
+    } else {
+        return lexicographic_sort(a, b);
+    }
+}
+
+int sort_by_length(const char* a, const char* b) {
+    int len_a = strlen(a);
+    int len_b = strlen(b);
+
+    if (len_a != len_b) {
+        return len_a - len_b;
+    } else {
+        return lexicographic_sort(a, b);
+    }
+}
+
+void string_sort(char** arr, const int len, int (*cmp_func)(const char* a, const char* b)) {
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (cmp_func(arr[i], arr[j]) > 0) {
+                char* temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+  
+    char** arr;
+	arr = (char**)malloc(n * sizeof(char*));
+  
+    for(int i = 0; i < n; i++){
+        *(arr + i) = malloc(1024 * sizeof(char));
+        scanf("%s", *(arr + i));
+        *(arr + i) = realloc(*(arr + i), strlen(*(arr + i)) + 1);
+    }
+  
+    string_sort(arr, n, lexicographic_sort);
+    for(int i = 0; i < n; i++)
+        printf("%s\n", arr[i]);
+    printf("\n");
+
+    string_sort(arr, n, lexicographic_sort_reverse);
+    for(int i = 0; i < n; i++)
+        printf("%s\n", arr[i]); 
+    printf("\n");
+
+    string_sort(arr, n, sort_by_length);
+    for(int i = 0; i < n; i++)
+        printf("%s\n", arr[i]);    
+    printf("\n");
+
+    string_sort(arr, n, sort_by_number_of_distinct_characters);
+    for(int i = 0; i < n; i++)
+        printf("%s\n", arr[i]); 
+    printf("\n");
+}
